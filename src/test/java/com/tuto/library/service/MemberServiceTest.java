463c23c7eda60b7575415a2eac09160704218ccd
GreenAladine6 <greenala139@gmail.com>
2025-09-09 10:19:03 +0100

    membernotfound

--- src/test/java/com/tuto/library/service/MemberServiceTest.java
+++ src/test/java/com/tuto/library/service/MemberServiceTest.java
@@ -1,11 +1,15 @@
 package com.tuto.library.service;
 
+import com.tuto.library.exception.MemberNotFoundException;
 import com.tuto.library.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 class MemberServiceTest {
@@ -18,5 +22,17 @@ class MemberServiceTest {
     @Test
     void shouldThrowMemberNotFoundException_whenDeletingNonExistentMember() {
         //TODO 1.0 : @Mohamed Sayed, implement this test
+        //GIVEN
+        given(memberRepository.findById("memberId1")).willReturn(Optional.empty());
+
+        //WHEN
+        Throwable thrown =catchThrowable(()-> {
+            memberService.deleteMember("memberId1");
+        });
+
+        //THEN
+        assertThat(thrown)
+                .isInstanceOf(MemberNotFoundException.class)
+                .hasMessageContaining("Member with ID memberId1 not found");
     }
 }
