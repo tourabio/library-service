7c8505297a5a20fe36132e91c87edd03eef34ffb
raed <ra3dvi@gmail.com>
2025-09-09 15:00:36 +0100

    add member exception test

--- src/test/java/com/tuto/library/service/MemberServiceTest.java
+++ src/test/java/com/tuto/library/service/MemberServiceTest.java
@@ -1,5 +1,10 @@
 package com.tuto.library.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.BDDAssertions.catchThrowable;
+import static org.mockito.BDDMockito.*;
+
+import com.tuto.library.exception.MemberNotFoundException;
 import com.tuto.library.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,8 +12,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 @ExtendWith(MockitoExtension.class)
 class MemberServiceTest {
+
     @Mock
     MemberRepository memberRepository;
 
@@ -17,6 +25,17 @@ class MemberServiceTest {
 
     @Test
     void shouldThrowMemberNotFoundException_whenDeletingNonExistentMember() {
-        //TODO 1.0 : @Mohamed Sayed, implement this test
+        // GIVEN: simulate that member does not exist
+        given(memberRepository.findById("member1")).willReturn(Optional.empty());
+
+        // WHEN: attempt to delete the member
+        Throwable thrown = catchThrowable(() -> memberService.deleteMember("member1"));
+
+        // THEN: verify the correct exception is thrown with correct message
+        assertThat(thrown)
+                .isInstanceOf(MemberNotFoundException.class)
+                .hasMessageContaining("Member with ID member1 not found");
     }
+
+    // You can add more tests here for successful deletion, etc.
 }
