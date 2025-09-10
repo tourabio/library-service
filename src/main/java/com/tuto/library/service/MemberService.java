3c6fdfd49f33eff89f5ddb652f468dd26f3b42de
yasmine <bouchamiyassssmin@gmail.com>
2025-09-10 14:19:47 +0100

    loanservice+BookService

--- src/main/java/com/tuto/library/service/MemberService.java
+++ src/main/java/com/tuto/library/service/MemberService.java
@@ -4,12 +4,7 @@ import com.tuto.library.domain.Member;
 import com.tuto.library.exception.MemberNotFoundException;
 import com.tuto.library.repository.MemberRepository;
 
-public class MemberService {
-    private final MemberRepository memberRepository;
-
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+public record MemberService(MemberRepository memberRepository) {
 
     public Member updateMember(Member member) {
         checkMemberExists(member.getId());
--- src/test/java/com/tuto/library/service/BookServiceTest.java
+++ src/test/java/com/tuto/library/service/BookServiceTest.java
@@ -1,11 +1,19 @@
 package com.tuto.library.service;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+import static org.mockito.BDDMockito.*;
+import com.tuto.library.domain.Book;
+import com.tuto.library.exception.BookNotAvailableException;
+import com.tuto.library.exception.BookNotFoundException;
+import com.tuto.library.exception.InvalidLoanOperationException;
 import com.tuto.library.repository.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.Optional;
 
 //TODO 2: implement the tests below using TDD approach
 @ExtendWith(MockitoExtension.class)
@@ -19,25 +27,87 @@ class BookServiceTest {
     @Test
     void shouldThrowBookNotFoundException_whenReturningBookByIdThatDoesNotExist() {
         //TODO 2.0: @Mohamed sayed, implement this test
+        //given
+        given(bookRepository.findById("book1")).willReturn(Optional.empty());
+
+        //when
+        Throwable thrown = catchThrowable(() -> bookService.returnBook("book1"));
+        //then
+        assertThat(thrown)
+                .isInstanceOf(BookNotFoundException.class)
+                .hasMessageContaining("Book with ID book1 not found");
+
     }
 
     @Test
     void shouldReturnTrue_whenBookIsAvailable() {
+        //Given
+        Book book = new Book("book1","atomic habits","James Clear",5);
+        given(bookRepository.findById("book1")).willReturn(Optional.of(book));
+        //when
+        boolean available = bookService.isAvailable("book1");
+        //then
+        assertThat(available).isTrue();
     }
 
     @Test
     void shouldDecreaseAvailableCopies_whenBorrowingBook() {
+        //Given
+        Book book = new Book("book1","atomic habits","James Clear",5);
+        given(bookRepository.findById("book1")).willReturn(Optional.of(book));
+        //when
+        bookService.borrowBook("book1");
+        //then
+        assertThat(book.getAvailableCopies()).isEqualTo(4);
     }
 
     @Test
     void shouldThrowBookNotAvailableException_whenBorrowingBookWithNoCopies() {
+        Book book = new Book("book1", "Atomic Habits", "James Clear", 1);
+        book.setAvailableCopies(0);
+
+        given(bookRepository.findById("book1")).willReturn(Optional.of(book));
+
+        // when
+        Throwable thrown = catchThrowable(() -> bookService.borrowBook("book1"));
+
+        // then
+        assertThat(thrown)
+                .isInstanceOf(BookNotAvailableException.class)
+                .hasMessage("Book with ID book1 has no available copies");
     }
+
 
     @Test
     void shouldIncreaseAvailableCopies_whenReturningBook() {
+        //Given
+        Book book = new Book("book1", "Atomic Habits", "James Clear", 5);
+        book.setAvailableCopies(3);
+        when(bookRepository.findById("book1")).thenReturn(Optional.of(book));
+        //when
+        bookService.returnBook("book1");
+        //then
+        assertThat(book.getAvailableCopies()).isEqualTo(4);
+        verify(bookRepository).save(book);
+
+
     }
 
     @Test
     void shouldThrowInvalidLoanOperationException_whenReturningBookWithMaxCopies() {
+        // Given
+        Book book = new Book("book1", "Atomic Habits", "James Clear", 3);
+        book.setAvailableCopies(3);
+
+        given(bookRepository.findById("book1")).willReturn(Optional.of(book));
+
+        // When
+        Throwable thrown = catchThrowable(() -> bookService.returnBook("book1"));
+
+        // Then
+        assertThat(thrown)
+                .isInstanceOf(InvalidLoanOperationException.class)
+                .hasMessageContaining("Cannot return book with ID book1 because copies are already at maximum");
+
     }
 }
