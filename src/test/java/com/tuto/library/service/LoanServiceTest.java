5ca3b747e6a989025656b291213a3575633d2038
safa nasri <safanasri002@gmail.com>
2025-09-12 14:52:07 +0100

    Loan service TODOs

--- src/test/java/com/tuto/library/service/LoanServiceTest.java
+++ src/test/java/com/tuto/library/service/LoanServiceTest.java
@@ -1,9 +1,13 @@
 package com.tuto.library.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.BDDMockito.*;
 import com.tuto.library.domain.Loan;
 import com.tuto.library.domain.LoanStatus;
+import com.tuto.library.exception.BookNotFoundException;
+import com.tuto.library.exception.InvalidLoanOperationException;
+import com.tuto.library.exception.LoanNotFoundException;
 import com.tuto.library.repository.LoanRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -41,5 +45,17 @@ class LoanServiceTest {
     @Test
     void shouldThrowInvalidLoanOperationException_whenReturningNonActiveLoan() {
         //TODO 1 : implement this test
+        //given
+        Loan loan = new Loan("loan1", "book1", "member1", LocalDate.now());
+        loan.setStatus(LoanStatus.RETURNED);
+        given(loanRepository.findById("loan1")).willReturn(Optional.of(loan));
+
+        //when
+        Throwable thrown = catchThrowable(() -> loanService.validateLoanIsActive("loan1",loan));
+
+        //then
+        assertThat(thrown)
+                .isInstanceOf(InvalidLoanOperationException.class)
+                .hasMessageContaining("Loan with ID loan1 is not active.");
     }
 }
