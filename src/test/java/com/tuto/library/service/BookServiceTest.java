a1715cacd48e257423dedece93048212782c3985
youssef <youssef.kadob@gmail.com>
2025-09-13 11:10:44 +0100

    loan and book test

--- src/test/java/com/tuto/library/service/BookServiceTest.java
+++ src/test/java/com/tuto/library/service/BookServiceTest.java
@@ -1,11 +1,18 @@
 package com.tuto.library.service;
 
+import com.tuto.library.exception.BookNotFoundException;
 import com.tuto.library.repository.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.BDDAssertions.catchThrowable;
+import static org.mockito.BDDMockito.given;
 
 //TODO 2: implement the tests below using TDD approach
 @ExtendWith(MockitoExtension.class)
@@ -15,14 +22,25 @@ class BookServiceTest {
 
     @InjectMocks
     private BookService bookService;
+    LoanService loanService;
 
     @Test
     void shouldThrowBookNotFoundException_whenReturningBookByIdThatDoesNotExist() {
         //TODO 2.0: @Mohamed sayed, implement this test
+        //given
+        given(bookRepository.findById("book1")).willReturn(Optional.empty());
+        //when
+        Throwable thrown = catchThrowable(() -> loanService.processLoanReturn("loan1"));
+        assertThat(thrown)
+                .isInstanceOf(BookNotFoundException.class)
+                .hasMessageContaining("book1");
+
+
     }
 
     @Test
     void shouldReturnTrue_whenBookIsAvailable() {
+
     }
 
     @Test
